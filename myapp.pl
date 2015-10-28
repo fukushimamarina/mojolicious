ed17fcd6223d0d21ea1d07f4fc09d27dabda983d
marinafukushima <marina.fukushima@gaiax.com>
2015-10-29 02:06:41 +0900

    フォームつくる

--- myapp.pl
+++ myapp.pl
@@ -5,8 +5,18 @@ use Mojolicious::Lite;
 # plugin 'PODRenderer';
 
 get '/' => sub {
-  my $c = shift;
-  $c->render(template => 'index');
+	my $c = shift;
+	$c->render(template => 'index');
+};
+
+post '/post' => sub {
+	my $c = shift;
+
+	my $name = $c->req->parameters->{name};
+	my $category = $c->req->parameters->{category};
+
+
+	$c->rendirect('/');
 };
 
 app->start;
@@ -16,8 +26,20 @@ __DATA__
 % layout 'default';
 % title 'エンジニア飲み会リスト';
 <h1>飲み会リスト</h1>
-飲み会のリストをつくります！
-<%= link_to 'here' => '/perldoc' %>.
+飲み会のリストをつくります！<br>
+元データはこちらをご参照ください！<%= link_to 'スプレッドシート' => 'https://docs.google.com/spreadsheets/d/1pAXG5zc96CAC2aUpw76-A0hayWs6cxVjpz9ZruKUBI0/edit#gid=0' %><br>
+
+<form class="form-inline" method="POST" action="/post">
+    <div class="form-group">
+        <label>お店の名前</label>
+        <input type="text" class="form-control" name="name" placeholder="例: マルミチェ">
+    </div>
+    <div class="form-group">
+        <label>カテゴリ</label>
+        <input type="text" class="form-control" name="category" placeholder="イタリアン／ワイン">
+    </div>
+    <button type="submit" class="btn btn-default">教える</button>
+</form>
 
 
 @@ layouts/default.html.ep
