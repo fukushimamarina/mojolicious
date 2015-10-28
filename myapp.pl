8876cae7bc85ef83133717e9da3ad15511763b69
marinafukushima <marina.fukushima@gaiax.com>
2015-10-29 02:33:46 +0900

    paramエラー対処とリストページ作成

--- myapp.pl
+++ myapp.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
+use Mojo::Message::Request;
+
 
 # Documentation browser under "/perldoc"
 # plugin 'PODRenderer';
@@ -12,11 +14,16 @@ get '/' => sub {
 post '/post' => sub {
 	my $c = shift;
 
-	my $name = $c->req->parameters->{name};
-	my $category = $c->req->parameters->{category};
+	my $name		= $c->param('name');
+	my $category	= $c->param('category');
 
+	$c->redirect_to('/list');
+};
 
-	$c->rendirect('/');
+get '/list' => sub {
+	my $c = shift;
+
+	$c->render(templete => 'list');
 };
 
 app->start;
@@ -25,7 +32,7 @@ __DATA__
 @@ index.html.ep
 % layout 'default';
 % title 'エンジニア飲み会リスト';
-<h1>飲み会リスト</h1>
+<h1>みんなでつくる！飲み会リスト</h1>
 飲み会のリストをつくります！<br>
 元データはこちらをご参照ください！<%= link_to 'スプレッドシート' => 'https://docs.google.com/spreadsheets/d/1pAXG5zc96CAC2aUpw76-A0hayWs6cxVjpz9ZruKUBI0/edit#gid=0' %><br>
 
@@ -40,6 +47,14 @@ __DATA__
     </div>
     <button type="submit" class="btn btn-default">教える</button>
 </form>
+
+@@ list.html.ep
+% layout 'default';
+% title 'エンジニア飲み会リスト';
+<h1>飲み会リスト一覧</h1>
+<hr>
+
+
 
 
 @@ layouts/default.html.ep
