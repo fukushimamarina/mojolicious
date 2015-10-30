568567dba80e218ecfd267f35b16527c3d1967c0
marinafukushima <marina.fukushima@gaiax.com>
2015-10-30 13:50:09 +0900

    入力して一覧にできるところまでは完了

--- myapp.pl
+++ myapp.pl
@@ -3,23 +3,31 @@ use Mojolicious::Lite;
 
 # Documentation browser under "/perldoc"
 # plugin 'PODRenderer';
+plugin 'TagHelpers';
 
 get '/' => sub {
 	my $c = shift;
 
-	my $name		= $c->param('name');
-	my $category	= $c->param('category');
-	$c->render('index',
-		name		=> $name,
-		category	=> $category
-	);
+	$c->render(template => 'index');
 };
 
 post '/post' => sub {
 	my $c = shift;
 
-	$c->redirect_to('/');
-};
+	my $name = $c->param('name');
+	my $category = $c->param('category');
+
+	$c->stash('name' => $name);
+	$c->stash('category' => $category);
+
+#	$c->redirect_to('/list');
+	$c->render();
+} => 'list';
+
+#get '/list' => sub {
+#	my $c = shift;
+#	$c->render(templete => 'list');
+#};
 
 
 app->start;
@@ -28,41 +36,51 @@ __DATA__
 @@ index.html.ep
 % layout 'default';
 % title 'エンジニア飲み会リスト';
+
 <h1>みんなでつくる！飲み会リスト</h1>
 飲み会のリストをつくります！<br>
 元データはこちらをご参照ください！<%= link_to 'スプレッドシート' => 'https://docs.google.com/spreadsheets/d/1pAXG5zc96CAC2aUpw76-A0hayWs6cxVjpz9ZruKUBI0/edit#gid=0' %><br>
 
-<form class="form-inline" method="POST" action="/post">
-    <div class="form-group">
-        <label>お店の名前</label>
-        <input type="text" class="form-control" name="name" placeholder="例: マルミチェ">
-    </div>
-    <div class="form-group">
-        <label>カテゴリ</label>
-        <input type="text" class="form-control" name="category" placeholder="イタリアン／ワイン">
-    </div>
-    <button type="submit" class="btn btn-default">教える</button>
-</form>
-<hr>
-<h2>飲み会リスト一覧</h2>
-一覧になってます。<br>
-<table border="1">
-	<tr><th>店名</th><th>カテゴリ</th></tr>
-	<tr><td>マルミチェ</td><td>イタリアン</td></tr>	
-	<tr><td><%= $name %></td><td><%= $category %></td></tr>
-</table>
+%= form_for post => (method => 'post') => begin
+	%= label_for name => '店名'
+	%= input_tag 'name', type => 'text', placeholder => 'マルミチェ'
+	%= label_for name => 'カテゴリー'
+	%= input_tag 'category', type => 'text', placeholder => 'イタリアン'
+	%= submit_button '追加する！'
+% end
 
+@@ list.html.ep
+% layout 'default';
+% title 'リスト一覧';
+
+<h1>飲み会リスト一覧</h1>
+一覧になってます。<br>
+
+<div class="list">
+<table class="table" border="1">
+	<thead>
+		<tr>
+			<th>店名</th>
+			<th>カテゴリ</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><%= $name %></td>
+			<td><%= $category %></td>
+		</tr>
+	</tbody>	
+</table>
+<div>
 
 
 @@ layouts/default.html.ep
 <!DOCTYPE html>
-<%
-  my $name = stash('name');
-  my $category = stash('category');
-%>
 <html>
 	<head>
 		<title><%= title %></title>
 	</head>
-	<body><%= content %></body>
+	<body>
+		<%= content %>
+	</body>
 </html>
